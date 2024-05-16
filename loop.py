fcd4faaeb689b0474f037bac1f5416873c3f52fe
Frankie <132312999+frankiephs@users.noreply.github.com>
2024-05-16 11:48:04 +0000

    added encoding and checks if final

--- loop.py
+++ loop.py
@@ -1,6 +1,17 @@
-# extract the data
-f = open("waka_ama_res/WakaNats2017/001-Heat 1-01.lif", "r")
+import os
 
-race = {'header' : f.readline(),
-        'body' : f.readline()}
-print(race)
+folder_path = "waka_ama_res/WakaNats2017"
+
+for filename in os.listdir(folder_path):
+
+        # Only extract finals files
+        if "Final" not in filename:
+                print("There is no final in", filename)
+                continue
+        else:
+                file_path = os.path.join(folder_path, filename)
+
+                with open(file_path, 'r', encoding='latin-1') as file:
+                        # Read all the contents of the file
+                        contents = file.read()
+                        print(contents)
