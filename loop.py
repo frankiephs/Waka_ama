5c22b4ea42cdf3e90218e6d1379fd3a5953b6c40
Frankie <132312999+frankiephs@users.noreply.github.com>
2024-05-16 12:23:41 +0000

    removed unneccessary commas

--- loop.py
+++ loop.py
@@ -6,23 +6,23 @@ disqualified_keywords = ['DNS', 'DQ', 'disqualified', 'Disqualified']
 
 for filename in os.listdir(folder_path):
         # Only extract finals files
-        """if "Final" not in filename:
+        if "Final" not in filename:
                 print("There is no final in", filename)
                 continue
-        else:"""
+        else:
 
-        file_path = os.path.join(folder_path, filename)
-
-        with open(file_path, 'r', encoding='latin-1') as file:
-                # Read all the contents of the file
-                content = file.read()
-
-                # categorizing into header and body components
-                page_category = {'header': '', 'body': []}
-                split_content = content.splitlines()
-                page_category['header'] = split_content.pop(0)
-                page_category['body'] = split_content
-                files_categorized.append(page_category)
+                file_path = os.path.join(folder_path, filename)
+        
+                with open(file_path, 'r', encoding='latin-1') as file:
+                        # Read all the contents of the file
+                        content = file.read()
+        
+                        # categorizing into header and body components
+                        page_category = {'header': '', 'body': []}
+                        split_content = content.splitlines()
+                        page_category['header'] = split_content.pop(0)
+                        page_category['body'] = split_content
+                        files_categorized.append(page_category)
 
 # open each files
 for i in files_categorized:
@@ -46,3 +46,14 @@ for i in files_categorized:
                 for word in disqualified_keywords:
                         if word in row:
                                 print("[!] ERROR: Row Disqualified.", row)
+
+        # scoring system main routine
+
+        # extract the row into a list, split by the comma
+        for row in body:
+                row_list = row.split(",")
+
+                # remove empty elements
+                while '' in row_list:
+                        row_list.remove('')
+                print(row_list)
